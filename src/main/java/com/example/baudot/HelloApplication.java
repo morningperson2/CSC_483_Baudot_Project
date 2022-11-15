d9c1d1bfe87972a447e9c0426a0727352e1dd2d3
Ethan Ison <isone1@nku.edu>
2022-11-15 18:29:56 -0500

    Added the addition table for the decoding/encoding functionality. Also a string formating function to eliminate white space in the plain and cipher texts. Tests for the add and format functions were also added.

--- src/main/java/com/example/baudot/HelloApplication.java
+++ src/main/java/com/example/baudot/HelloApplication.java
@@ -23,7 +23,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();*/
 
-        stage.setTitle("Baudot");
+        stage.setTitle("TextField test");
         Group root = new Group();
         Scene scene = new Scene(root, 650, 700, Color.WHITE);
 
@@ -74,7 +74,7 @@ public class HelloApplication extends Application {
         Button b2 = new Button("Encrypt");
         Button b3 = new Button("Decrypt");
 
-        EventHandler<ActionEvent> event = actionEvent -> pt.setText(ct1.getText()+ct2.getText());
+        EventHandler<ActionEvent> event = actionEvent -> pt.setText(add(ct1.getText(), ct2.getText()));
 
         EventHandler<ActionEvent> encrypt = actionEvent -> {
 
@@ -122,5 +122,157 @@ public class HelloApplication extends Application {
 
     public static TilePane cryptanalysis(){
         return new TilePane();
+    }
+
+    public static String add(String string1, String string2) {
+        int xValue = 0;
+        int yValue = 0;
+        String ans = new String();
+
+        String ct1 = formatString(string1);
+        String ct2 = formatString(string2);
+        char[][] additionTable = {
+                {'/', 'G', 'F', 'R', '4', 'C', 'B', 'Q', 'S', '3', 'N', 'Z', '8', 'K', '5', 'Y', 'H', 'D', 'I', 'W', '9', 'X', 'T', 'V',
+                        'P', 'L', 'M', 'O', 'J', 'E', 'U', 'A'},
+                {'G', '/', 'Q', 'T', 'O', 'H', 'A', 'F', '8', 'L', 'P', 'J', 'S', 'Y', 'E', 'K', 'C', 'W', 'M', 'D', 'V', 'U', 'R', '9',
+                        'N', '3', 'I', '4', 'Z', '5', 'X', 'B'},
+                {'F', 'Q', '/', 'U', 'K', 'A', 'H', 'G', '3', 'S', 'E', 'M', '4', 'L', 'P', 'O', 'B', '9', 'J', 'V', 'D', 'T', 'X', 'W',
+                        '5', '8', 'Z', 'Y', 'I', 'N', 'R', 'C'},
+                {'R', 'T', 'U', '/', '3', '9', 'W', 'X', 'K', '4', 'I', '5', 'Y', 'S', 'Z', '8', 'V', 'A', 'N', 'B', 'C', 'Q', 'G', 'H',
+                        'M', 'O', 'P', 'L', 'E', 'J', 'F', 'D'},
+                {'4', 'O', 'K', '3', '/', 'N', '5', 'Y', 'U', 'R', 'C', 'W', 'X', 'F', 'B', 'Q', 'P', 'J', '9', 'Z', 'I', '8', 'L', 'M',
+                        'H', 'T', 'V', 'G', 'D', 'A', 'S', 'E'},
+                {'C', 'H', 'A', '9', 'N', '/', 'Q', 'B', 'J', 'I', '4', '8', 'Z', 'E', 'Y', '5', 'G', 'U', '3', 'X', 'R', 'W', 'V', 'T',
+                        'O', 'M', 'L', 'P', 'S', 'K', 'D', 'F'},
+                {'B', 'A', 'H', 'W', '5', 'Q', '/', 'C', 'M', 'Z', 'Y', '3', 'I', 'P', '4', 'N', 'F', 'T', '8', 'R', 'X', '9', 'D', 'U',
+                        'K', 'J', 'S', 'E', 'L', 'O', 'V', 'G'},
+                {'Q', 'F', 'G', 'X', 'Y', 'B', 'C', '/', 'L', '8', '5', 'I', '3', 'O', 'N', '4', 'A', 'V', 'Z', '9', 'W', 'R', 'U', 'D',
+                        'E', 'S', 'J', 'K', 'M', 'P', 'T', 'H'},
+                {'S', '8', '3', 'K', 'U', 'J', 'M', 'L', '/', 'F', 'D', 'H', 'G', 'R', 'V', 'T', 'Z', 'N', 'A', 'P', 'E', 'O', 'Y', '5',
+                        'W', 'Q', 'B', 'X', 'C', '9', '4', 'I'},
+                {'3', 'L', 'S', '4', 'R', 'I', 'Z', '8', 'F', '/', '9', 'B', 'Q', 'U', 'W', 'X', 'M', 'E', 'C', '5', 'N', 'Y', 'O', 'P',
+                        'V', 'G', 'H', 'T', 'A', 'D', 'K', 'J'},
+                {'N', 'P', 'E', 'I', 'C', '4', 'Y', '5', 'D', '9', '/', 'X', 'W', 'A', 'Q', 'B', 'O', 'S', 'R', '8', '3', 'Z', 'M', 'L',
+                        'G', 'V', 'T', 'H', 'U', 'F', 'J', 'K'},
+                {'Z', 'J', 'M', '5', 'W', '8', '3', 'I', 'H', 'B', 'X', '/', 'C', 'V', 'R', '9', 'S', 'O', 'Q', '4', 'Y', 'N', 'E', 'K',
+                        'U', 'A', 'F', 'D', 'G', 'T', 'P', 'L'},
+                {'8', 'S', 'L', 'Y', 'X', 'Z', 'I', '3', 'G', 'Q', 'W', 'C', '/', 'T', '9', 'R', 'J', 'P', 'B', 'N', '5', '4', 'K', 'E',
+                        'D', 'F', 'A', 'U', 'H', 'V', 'O', 'M'},
+                {'K', 'Y', '4', 'S', 'F', 'E', 'P', 'O', 'R', 'U', 'A', 'V', 'T', '/', 'H', 'G', '5', 'I', 'D', 'M', 'J', 'L', '8', 'Z',
+                        'B', 'X', 'W', 'Q', '9', 'C', '3', 'N'},
+                {'5', 'E', 'P', 'Z', 'B', 'Y', '4', 'N', 'V', 'W', 'Q', 'R', '9', 'H', '/', 'C', 'K', 'L', 'X', '3', '8', 'I', 'J', 'S',
+                        'F', 'D', 'U', 'A', 'T', 'G', 'M', 'O'},
+                {'Y', 'K', 'O', '8', 'Q', '5', 'N', '4', 'T', 'X', 'B', '9', 'R', 'G', 'C', '/', 'E', 'M', 'W', 'I', 'Z', '3', 'S', 'J',
+                        'A', 'U', 'D', 'F', 'V', 'H', 'L', 'P'},
+                {'H', 'C', 'B', 'V', 'P', 'G', 'F', 'A', 'Z', 'M', 'O', 'S', 'J', '5', 'K', 'E', '/', 'X', 'L', 'U', 'T', 'D', '9', 'R',
+                        '4', 'I', '3', 'N', 'B', 'Y', 'W', 'Q'},
+                {'D', 'W', '9', 'A', 'J', 'U', 'T', 'V', 'N', 'E', 'S', 'O', 'P', 'I', 'L', 'M', 'X', '/', 'K', 'G', 'F', 'H', 'B', 'Q',
+                        '8', '5', 'Y', 'Z', '4', '3', 'C', 'R'},
+                {'I', 'M', 'J', 'N', '9', '3', '8', 'Z', 'A', 'C', 'R', 'Q', 'B', 'D', 'X', 'W', 'L', 'K', '/', 'Y', '4', '5', 'P', 'O',
+                        'T', 'H', 'G', 'V', 'F', 'U', 'E', 'S'},
+                {'W', 'D', 'V', 'B', 'Z', 'X', 'R', '9', 'P', '5', '8', '4', 'N', 'M', '3', 'I', 'U', 'G', 'Y', '/', 'Q', 'C', 'A', 'F',
+                        'S', 'E', 'K', 'J', 'O', 'L', 'H', 'T'},
+                {'9', 'V', 'D', 'C', 'I', 'R', 'X', 'W', 'E', 'N', '3', 'Y', '5', 'J', '8', 'Z', 'T', 'F', '4', 'Q', '/', 'B', 'H', 'G',
+                        'L', 'P', 'O', 'M', 'K', 'S', 'A', 'U'},
+                {'X', 'U', 'T', 'Q', '8', 'W', '9', 'R', 'O', 'Y', 'Z', 'N', '4', 'L', 'I', '3', 'D', 'H', '5', 'C', 'B', '/', 'F', 'A',
+                        'J', 'K', 'E', 'S', 'P', 'M', 'G', 'V'},
+                {'T', 'R', 'X', 'G', 'L', 'V', 'D', 'U', 'Y', 'O', 'M', 'E', 'K', '8', 'J', 'S', '9', 'B', 'P', 'A', 'H', 'F', '/', 'C',
+                        'I', '4', 'N', '3', '5', 'Z', 'Q', 'W'},
+                {'V', '9', 'W', 'H', 'M', 'T', 'U', 'D', '5', 'P', 'L', 'K', 'E', 'Z', 'S', 'J', 'R', 'Q', 'O', 'F', 'G', 'A', 'C', '/',
+                        '3', 'N', '4', 'I', 'Y', '8', 'B', 'X'},
+                {'P', 'N', '5', 'M', 'H', 'O', 'K', 'E', 'W', 'V', 'G', 'U', 'D', 'B', 'F', 'A', '4', '8', 'T', 'S', 'L', 'J', 'I', '3',
+                        '/', '9', 'R', 'C', 'X', 'Q', 'Z', 'Y'},
+                {'L', '3', '8', 'O', 'T', 'M', 'J', 'S', 'Q', 'G', 'V', 'A', 'F', 'X', 'D', 'U', 'I', '5', 'H', 'E', 'P', 'K', '4', 'N',
+                        '9', '/', 'C', 'R', 'B', 'W', 'Y', 'Z'},
+                {'M', 'I', 'Z', 'P', 'V', 'L', 'S', 'J', 'B', 'H', 'T', 'F', 'A', 'W', 'U', 'D', '3', 'Y', 'G', 'K', 'O', 'E', 'N', '4',
+                        'R', 'C', '/', '9', 'Q', 'X', '5', '8'},
+                {'O', '4', 'Y', 'L', 'G', 'P', 'E', 'K', 'X', 'T', 'H', 'D', 'U', 'Q', 'A', 'F', 'N', 'Z', 'V', 'J', 'M', 'S', '3', 'I',
+                        'C', 'R', '9', '/', 'W', 'B', '8', '5'},
+                {'J', 'Z', 'I', 'E', 'D', 'S', 'L', 'M', 'C', 'A', 'U', 'G', 'H', '9', 'T', 'V', '8', '4', 'F', 'O', 'K', 'P', '5', 'Y',
+                        'X', 'B', 'Q', 'W', '/', 'R', 'N', '3'},
+                {'E', '5', 'N', 'J', 'A', 'K', 'O', 'P', '9', 'D', 'F', 'T', 'V', 'C', 'G', 'H', 'Y', '3', 'U', 'L', 'S', 'M', 'Z', '8',
+                        'Q', 'W', 'X', 'B', 'R', '/', 'I', '4'},
+                {'U', 'X', 'R', 'F', 'S', 'D', 'V', 'T', '4', 'K', 'J', 'P', 'O', '3', 'M', 'L', 'W', 'C', 'E', 'H', 'A', 'G', 'Q', 'B',
+                        'Z', 'Y', '5', '8', 'N', 'I', '/', '9'},
+                {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                        'Y', 'Z', '8', '5', '3', '4', '9', '/'},
+        };
+
+
+        /*
+        for(int i = 0; i < ct1.length(); i++){
+            if(ct1.charAt(i) != ' ' && ct2.charAt(i) != ' ') {
+                //System.out.print(add(ct1.charAt(i), ct2.charAt(i)));
+                combinedCT += add(ct1.charAt(i), ct2.charAt(i));
+            }
+        }
+        */
+
+
+        for(int i = 0; i < ct1.length(); i++) {
+            if (Character.isUpperCase(ct1.charAt(i))) {
+                xValue = ct1.charAt(i) - 65;
+            } else if (Character.isLowerCase(ct1.charAt(i))) {
+                xValue = ct1.charAt(i) - 97;
+            } else {
+                if (ct1.charAt(i) == '8') {
+                    xValue = 26;
+                }
+                if (ct1.charAt(i) == '5') {
+                    xValue = 27;
+                }
+                if (ct1.charAt(i) == '3') {
+                    xValue = 28;
+                }
+                if (ct1.charAt(i) == '4') {
+                    xValue = 29;
+                }
+                if (ct1.charAt(i) == '9') {
+                    xValue = 30;
+                }
+                if (ct1.charAt(i) == '/') {
+                    xValue = 31;
+                }
+            }
+
+            if (Character.isUpperCase(ct2.charAt(i))) {
+                yValue = ct2.charAt(i) - 65;
+            } else if (Character.isLowerCase(ct2.charAt(i))) {
+                yValue = ct2.charAt(i) - 97;
+            } else {
+                if (ct2.charAt(i) == '8') {
+                    yValue = 26;
+                }
+                if (ct2.charAt(i) == '5') {
+                    yValue = 27;
+                }
+                if (ct2.charAt(i) == '3') {
+                    yValue = 28;
+                }
+                if (ct2.charAt(i) == '4') {
+                    yValue = 29;
+                }
+                if (ct2.charAt(i) == '9') {
+                    yValue = 30;
+                }
+                if (ct2.charAt(i) == '/') {
+                    yValue = 31;
+                }
+            }
+
+            ans = ans + additionTable[xValue][yValue];
+        }
+        return ans;
+
+    }
+
+    public static String formatString(String string){
+        String tmp = new String();
+        for(int i = 0; i < string.length(); i++){
+            if(string.charAt(i) != ' ') {
+                tmp = tmp + string.charAt(i);
+            }
+        }
+        return tmp;
     }
 }
